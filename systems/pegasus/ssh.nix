a518f87940e090665efa44eabfc49bc467ee54ef
Sizhe Zhao <prc.zhao@outlook.com>
2024-11-08 19:43:52 +0800

    feat(systems/pegasus/ssh): add extra config

--- systems/pegasus/ssh.nix
+++ systems/pegasus/ssh.nix
@@ -1,6 +1,10 @@
+{ config, ... }:
 {
   services.openssh = {
     enable = true;
     settings.AuthenticationMethods = "publickey";
+    extraConfig = ''
+      Include ${config.sops.templates.openssh.path}
+    '';
   };
 }
