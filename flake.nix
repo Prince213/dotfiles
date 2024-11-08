368433cdfe98fa602f8ef524500b76f321a2e26a
Sizhe Zhao <prc.zhao@outlook.com>
2024-11-08 17:33:01 +0800

    feat(flake): add disko

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, disko, ... }:
     {
       nixosConfigurations = {
         pegasus = nixpkgs.lib.nixosSystem {
