e6c31ad05780a77087a90f21027e6c5675807bb2
Sizhe Zhao <prc.zhao@outlook.com>
2024-11-24 16:59:50 +0800

    feat(systems/apus/networking): set connection uuid

--- systems/apus/networking.nix
+++ systems/apus/networking.nix
@@ -8,6 +8,7 @@
         SUSTech_5G = {
           connection = {
             id = "SUSTech_5G";
+            uuid = "1d3acb48-0d12-425f-a5f7-108b0d5049b4";
             type = "wifi";
             autoconnect = true;
           };
