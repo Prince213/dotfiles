aac3b387a1042df2537b189fd561843398080e9c
Sizhe Zhao <prc.zhao@outlook.com>
2024-11-28 21:49:56 +0800

    feat(flake): add home-manager

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
