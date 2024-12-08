cee41d0baa9d39e15b3153402ccccc2b1790d313
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-08 19:07:08 +0800

    feat(systems/pegasus/networking): disable firewall

--- systems/pegasus/networking.nix
+++ systems/pegasus/networking.nix
@@ -1,21 +1,25 @@
 { config, ... }:
 {
-  networking.networkmanager = {
-    enable = true;
-    ensureProfiles = {
-      environmentFiles = [ config.sops.templates.networkmanager.path ];
-      profiles = {
-        ShanghaiTech-IoT = {
-          connection = {
-            id = "ShanghaiTech-IoT";
-            uuid = "8eb2a392-c5b9-4083-9d9c-38c0d9027146";
-            type = "wifi";
-            autoconnect = true;
-          };
-          wifi.ssid = "ShanghaiTech-IoT";
-          wifi-security = {
-            key-mgmt = "wpa-psk";
-            psk = "$PSK_ShanghaiTech_IoT";
+  networking = {
+    firewall.enable = false;
+
+    networkmanager = {
+      enable = true;
+      ensureProfiles = {
+        environmentFiles = [ config.sops.templates.networkmanager.path ];
+        profiles = {
+          ShanghaiTech-IoT = {
+            connection = {
+              id = "ShanghaiTech-IoT";
+              uuid = "8eb2a392-c5b9-4083-9d9c-38c0d9027146";
+              type = "wifi";
+              autoconnect = true;
+            };
+            wifi.ssid = "ShanghaiTech-IoT";
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$PSK_ShanghaiTech_IoT";
+            };
           };
         };
       };
