5e0b277a457f6ceddd93b8eaa2f719a2d3486c05
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-16 00:36:22 +0800

    feat(homes/apus/wezterm): init config

--- homes/apus/wezterm.nix
+++ homes/apus/wezterm.nix
@@ -3,5 +3,11 @@
   programs.wezterm = {
     enable = true;
     package = wezterm.packages.${pkgs.system}.default;
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local config = wezterm.config_builder()
+
+      return config
+    '';
   };
 }
