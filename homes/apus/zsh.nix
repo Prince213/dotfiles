1066eabffd83e6e98c0809bef2850cd36e5a9d1b
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-15 22:20:27 +0800

    feat(homes/apus/zsh): setup powerlevel10k

--- homes/apus/zsh.nix
+++ homes/apus/zsh.nix
@@ -16,5 +16,13 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
     ];
+    initExtraFirst = ''
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
+    initExtra = ''
+      source ${./p10k.zsh}
+    '';
   };
 }
