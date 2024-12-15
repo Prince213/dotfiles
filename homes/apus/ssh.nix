771cd9a2a7980648416ded8577ca81d41dccf7e6
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-16 02:50:04 +0800

    feat(homes/apus/ssh): add config

--- homes/apus/ssh.nix
+++ homes/apus/ssh.nix
@@ -1,7 +1,9 @@
+{ config, ... }:
 {
   programs.ssh = {
     enable = true;
     addKeysToAgent = "1h";
+    includes = [ config.sops.secrets.ssh-config.path ];
   };
 
   services.ssh-agent.enable = true;
