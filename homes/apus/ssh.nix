0c171026c8631dd965b065a84b6c1798fb9b7692
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-16 02:50:24 +0800

    feat(homes/apus/ssh): add nun

--- homes/apus/ssh.nix
+++ homes/apus/ssh.nix
@@ -4,6 +4,14 @@
     enable = true;
     addKeysToAgent = "1h";
     includes = [ config.sops.secrets.ssh-config.path ];
+    matchBlocks = {
+      nun = {
+        host = "nun";
+        user = "root";
+        identityFile = config.sops.secrets.nun-ssh-root-key.path;
+        identitiesOnly = true;
+      };
+    };
   };
 
   services.ssh-agent.enable = true;
