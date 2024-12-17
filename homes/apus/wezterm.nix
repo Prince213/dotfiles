ae131a5936e80583231f414eb489df10ea950886
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-17 14:18:12 +0800

    feat(homes/apus/wezterm): hide tab bar if only one tab

--- homes/apus/wezterm.nix
+++ homes/apus/wezterm.nix
@@ -7,6 +7,8 @@
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
 
+      config.hide_tab_bar_if_only_one_tab = true
+
       config.font = wezterm.font_with_fallback {
         'Iosevka',
       }
