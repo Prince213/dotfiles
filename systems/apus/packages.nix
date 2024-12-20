6ab326630edce8064873477eeb78f6e132877cf6
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-20 16:53:13 +0800

    feat(systems/apus/packages): add steam

--- systems/apus/packages.nix
+++ systems/apus/packages.nix
@@ -1,7 +1,17 @@
+{ lib, ... }:
 {
   hardware.bluetooth.enable = true;
+
+  programs.steam.enable = true;
 
   services.tailscale.enable = true;
 
   programs.zsh.enable = true;
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-unwrapped"
+    ];
 }
