fde9d02c7a79d8593c28c3d3529fd9662f3263a3
Sizhe Zhao <prc.zhao@outlook.com>
2025-01-13 14:38:54 +0800

    feat(systems/apus/networking): open kdeconnect port

--- systems/apus/networking.nix
+++ systems/apus/networking.nix
@@ -1,9 +1,23 @@
 { config, ... }:
 {
   networking.firewall = {
+    allowedTCPPortRanges = [
+      # kdeconnect
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
     allowedTCPPorts = [
       # localsend
       53317
+    ];
+    allowedUDPPortRanges = [
+      # kdeconnect
+      {
+        from = 1714;
+        to = 1764;
+      }
     ];
     allowedUDPPorts = [
       # localsend
