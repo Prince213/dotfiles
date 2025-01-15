095c31f4e89c55c6971e642e2d68f0d118a01e32
Sizhe Zhao <prc.zhao@outlook.com>
2025-01-15 20:30:43 +0800

    feat(clang-format): init

--- treefmt.nix
+++ treefmt.nix
@@ -5,10 +5,12 @@
     "LICENSE"
     "README.md"
     ".envrc"
+    ".clang-format"
     "homes/apus/p10k.zsh"
   ];
 
   programs = {
+    clang-format.enable = true;
     nixfmt.enable = true;
     yamlfmt.enable = true;
   };
