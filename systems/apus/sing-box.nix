f0b1ee56bc03300b0b11cf2e49ec30b9cfa972aa
Sizhe Zhao <prc.zhao@outlook.com>
2025-01-19 00:39:43 +0800

    feat(systems/apus/sing-box): add inbound

--- systems/apus/sing-box.nix
+++ systems/apus/sing-box.nix
@@ -7,6 +7,13 @@
       log = {
         level = "info";
       };
+      inbounds = [
+        {
+          type = "mixed";
+          listen = "127.0.0.1";
+          listen_port = 2001;
+        }
+      ];
     };
   };
 }
