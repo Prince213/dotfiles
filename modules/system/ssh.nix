4a29850318b0960acbcbc91ee3bd25050a4491d8
Sizhe Zhao <prc.zhao@outlook.com>
2025-01-21 18:10:12 +0800

    feat(modules/system/ssh): only enable ed25519 host key

--- modules/system/ssh.nix
+++ modules/system/ssh.nix
@@ -7,5 +7,11 @@
       KbdInteractiveAuthentication = lib.mkDefault false;
       PasswordAuthentication = lib.mkDefault false;
     };
+    hostKeys = lib.mkDefault [
+      {
+        type = "ed25519";
+        path = "/etc/ssh/ssh_host_ed25519_key";
+      }
+    ];
   };
 }
