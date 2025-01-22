2e001a40f237c0144e55f7bca2dd7e7d74b8571e
Sizhe Zhao <prc.zhao@outlook.com>
2025-01-22 22:21:32 +0800

    feat(systems/pegasus/boot): enable dhcp for wlan

--- systems/pegasus/boot.nix
+++ systems/pegasus/boot.nix
@@ -9,6 +9,10 @@
       enable = true;
       network = {
         enable = true;
+        networks."10-wlan0" = {
+          matchConfig.Name = "wlan0";
+          DHCP = "yes";
+        };
       };
     };
   };
