e08e9744bc091579632a99bd885eba11d6319520
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-08 12:18:05 +0800

    feat(flake): don't follow nixpkgs for neovim-nightly

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     neovim-nightly = {
       url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
