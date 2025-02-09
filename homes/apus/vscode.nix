2e477e754abbb81858f4348a41753450cd8b7bef
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-09 16:18:24 +0800

    feat(homes/apus/vscode): use iosevka ss08

--- homes/apus/vscode.nix
+++ homes/apus/vscode.nix
@@ -4,6 +4,7 @@
     enable = true;
     package = pkgs.vscodium;
     userSettings = {
+      "editor.fontFamily" = "Iosevka SS08";
       "editor.rulers" = [ 80 ];
       "window.commandCenter" = false;
       "window.titleBarStyle" = "custom";
