c62608512536e38a4cd43985b42ae8706c7b784c
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-09 16:22:44 +0800

    feat(homes/apus/vscode): immutable extensions

--- homes/apus/vscode.nix
+++ homes/apus/vscode.nix
@@ -3,6 +3,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    mutableExtensionsDir = false;
     userSettings = {
       "editor.fontFamily" = "Iosevka SS08";
       "editor.fontLigatures" = true;
