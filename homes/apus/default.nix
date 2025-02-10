d109360e424436c1d6a831f020516b6261bf7e99
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-10 12:20:06 +0800

    feat(homes/apus): unset home info

--- homes/apus/default.nix
+++ homes/apus/default.nix
@@ -1,11 +1,5 @@
-{ config, ... }:
 {
   home.stateVersion = "24.11";
-
-  home = {
-    username = "prince213";
-    homeDirectory = "/home/${config.home.username}";
-  };
 
   programs.home-manager.enable = true;
 
