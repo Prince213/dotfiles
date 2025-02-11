c20d7d956c09f12572e77ddb9bb63d7e92434ce1
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-12 03:27:47 +0800

    feat(homes/apus/tmux): add navi

--- homes/apus/tmux.nix
+++ homes/apus/tmux.nix
@@ -2,5 +2,10 @@
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
+    extraConfig = ''
+      bind-key -N "Open Navi" -T prefix C-g split-window \
+        "$SHELL -l -i -c 'navi --print | tmux load-buffer -b tmp - && \
+          tmux paste-buffer -d -p -b tmp -t {last}'"
+    '';
   };
 }
