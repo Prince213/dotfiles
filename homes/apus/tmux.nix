31afc5d345b11ec93311ece406cfb7ed40d24cd9
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-12 13:38:04 +0800

    feat(homes/apus/tmux): allow passthrough

--- homes/apus/tmux.nix
+++ homes/apus/tmux.nix
@@ -3,6 +3,8 @@
     enable = true;
     terminal = "tmux-256color";
     extraConfig = ''
+      set-option -g allow-passthrough on
+
       bind-key -N "Open Navi" -T prefix C-g split-window \
         "$SHELL -l -i -c 'navi --print | tmux load-buffer -b tmp - && \
           tmux paste-buffer -d -p -b tmp -t {last}'"
