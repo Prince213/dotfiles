39974404ef134d2d0f31dc1c23dbf8c951522762
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-15 20:19:32 +0800

    feat(modules/system/firewalld): add enable

--- modules/system/firewalld.nix
+++ modules/system/firewalld.nix
@@ -1,2 +1,13 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.services.firewalld;
+in
 {
+  options.services.firewalld = {
+    enable = lib.mkEnableOption "FirewallD";
+  };
+
+  config = lib.mkIf cfg.enable {
+  };
 }
