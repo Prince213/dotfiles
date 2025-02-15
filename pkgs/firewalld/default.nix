9d23c715f99dd13bd435571b1748f8f8966e258b
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-15 18:38:51 +0800

    feat(pkgs/firewalld): add iptables

--- pkgs/firewalld/default.nix
+++ pkgs/firewalld/default.nix
@@ -3,6 +3,7 @@
   fetchFromGitHub,
   autoreconfHook,
   python3,
+  iptables,
   libxml2,
   libxslt,
   glib,
@@ -29,6 +30,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     autoreconfHook
     python3
+    iptables
     libxml2
     libxslt
     glib
