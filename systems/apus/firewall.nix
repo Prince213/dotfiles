9a94ed6429639bd0770eeab4770ca5e04b277e4e
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-15 22:19:53 +0800

    feat(systems/apus/firewall): use firewalld

--- systems/apus/firewall.nix
+++ systems/apus/firewall.nix
@@ -1,26 +1,31 @@
 {
+  services.firewalld = {
+    enable = true;
+  };
+
   networking.firewall = {
-    allowedTCPPortRanges = [
-      # kdeconnect
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-    allowedTCPPorts = [
-      # localsend
-      53317
-    ];
-    allowedUDPPortRanges = [
-      # kdeconnect
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-    allowedUDPPorts = [
-      # localsend
-      53317
-    ];
+    enable = false;
+    # allowedTCPPortRanges = [
+    #   # kdeconnect
+    #   {
+    #     from = 1714;
+    #     to = 1764;
+    #   }
+    # ];
+    # allowedTCPPorts = [
+    #   # localsend
+    #   53317
+    # ];
+    # allowedUDPPortRanges = [
+    #   # kdeconnect
+    #   {
+    #     from = 1714;
+    #     to = 1764;
+    #   }
+    # ];
+    # allowedUDPPorts = [
+    #   # localsend
+    #   53317
+    # ];
   };
 }
