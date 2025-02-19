dae9870c309197879d1e1f99649599e2b658c42d
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-20 02:35:25 +0800

    feat(systems/orion/disk): add disk main

--- systems/orion/disk.nix
+++ systems/orion/disk.nix
@@ -1,6 +1,32 @@
 {
   disko.devices = {
-    disk = {
+    disk.main = {
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            priority = 1;
+            type = "ef00";
+            size = "1G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            priority = 2;
+            type = "8304";
+            size = "100%";
+            content = {
+              type = "btrfs";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
     };
   };
 }
