33b97b38e2ff3dd6cad65a777080e9d0d0b87b0f
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-20 04:05:55 +0800

    feat(systems/orion/networking): add SUSTech_5G

--- systems/orion/networking.nix
+++ systems/orion/networking.nix
@@ -4,6 +4,21 @@
     enable = true;
     ensureProfiles = {
       environmentFiles = [ config.sops.templates.networkmanager.path ];
+      profiles = {
+        SUSTech_5G = {
+          connection = {
+            id = "SUSTech_5G";
+            uuid = "1d3acb48-0d12-425f-a5f7-108b0d5049b4";
+            type = "wifi";
+            autoconnect = true;
+          };
+          wifi.ssid = "SUSTech_5G";
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk = "$PSK_SUSTech_5G";
+          };
+        };
+      };
     };
   };
 }
