911b73c4ef0f72a2cade06293ca9e166e0761fc2
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-20 03:46:52 +0800

    feat(systems/orion/boot): add broadcom drivers

--- systems/orion/boot.nix
+++ systems/orion/boot.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
 {
   hardware.cpu.amd.updateMicrocode = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.broadcom_sta
+  ];
 }
--- systems/orion/packages.nix
+++ systems/orion/packages.nix
@@ -1,5 +1,12 @@
+{ lib, ... }:
 {
   hardware.enableRedistributableFirmware = true;
 
   services.tailscale.enable = true;
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "broadcom-sta"
+    ];
 }
