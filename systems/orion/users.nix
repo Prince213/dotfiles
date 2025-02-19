1634b61cc93f21de55be98159a061daa2497b700
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-20 03:07:40 +0800

    feat(systems/orion/users): set root ssh key

--- systems/orion/users.nix
+++ systems/orion/users.nix
@@ -3,6 +3,9 @@
   users.users = {
     root = {
       hashedPasswordFile = config.sops.secrets.root-password-hash.path;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOHYSjr4W32QSU6SGdwOskpOaf1blfWDhhG7ImnvBxVi"
+      ];
     };
   };
 }
