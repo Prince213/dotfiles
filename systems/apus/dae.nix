890e21b6f60118f56e5509c80d78d4f68a098c5e
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-23 01:39:37 +0800

    feat(systems/apus/dae): toggle on network switch

--- systems/apus/dae.nix
+++ systems/apus/dae.nix
@@ -1,4 +1,9 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   services.dae = {
     enable = true;
@@ -8,4 +13,25 @@
     };
     configFile = config.sops.secrets.dae.path;
   };
+
+  networking.networkmanager.dispatcherScripts =
+    let
+      systemctl = lib.getExe' pkgs.systemdMinimal "systemctl";
+    in
+    [
+      {
+        type = "basic";
+        source = pkgs.writeShellScript "start-dae" ''
+          if test "x$2" = "xup"; then
+            ${systemctl} start dae.service
+          fi
+        '';
+      }
+      {
+        type = "pre-down";
+        source = pkgs.writeShellScript "stop-dae" ''
+          ${systemctl} stop dae.service
+        '';
+      }
+    ];
 }
