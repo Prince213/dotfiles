237f719f4eed063ac87d7ec4df3e05e4590b2957
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-23 18:49:48 +0800

    feat(systems/apus/dae): disable

--- systems/apus/dae.nix
+++ systems/apus/dae.nix
@@ -1,12 +1,12 @@
 {
   config,
-  lib,
+  # lib,
   pkgs,
   ...
 }:
 {
   services.dae = {
-    enable = true;
+    # enable = true;
     package = pkgs.dae-beta;
     openFirewall = {
       enable = true;
@@ -15,24 +15,24 @@
     configFile = config.sops.secrets.dae.path;
   };
 
-  networking.networkmanager.dispatcherScripts =
-    let
-      systemctl = lib.getExe' pkgs.systemdMinimal "systemctl";
-    in
-    [
-      {
-        type = "basic";
-        source = pkgs.writeShellScript "start-dae" ''
-          if test "x$2" = "xup"; then
-            ${systemctl} start dae.service
-          fi
-        '';
-      }
-      {
-        type = "pre-down";
-        source = pkgs.writeShellScript "stop-dae" ''
-          ${systemctl} stop dae.service
-        '';
-      }
-    ];
+  # networking.networkmanager.dispatcherScripts =
+  #   let
+  #     systemctl = lib.getExe' pkgs.systemdMinimal "systemctl";
+  #   in
+  #   [
+  #     {
+  #       type = "basic";
+  #       source = pkgs.writeShellScript "start-dae" ''
+  #         if test "x$2" = "xup"; then
+  #           ${systemctl} start dae.service
+  #         fi
+  #       '';
+  #     }
+  #     {
+  #       type = "pre-down";
+  #       source = pkgs.writeShellScript "stop-dae" ''
+  #         ${systemctl} stop dae.service
+  #       '';
+  #     }
+  #   ];
 }
