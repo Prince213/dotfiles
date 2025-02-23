fbd1f38350f2e3b65c8c2af3c2def9d2a1ee7a63
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-23 21:50:11 +0800

    feat(homes/apus/vscode): add default profile

--- homes/apus/vscode.nix
+++ homes/apus/vscode.nix
@@ -4,26 +4,28 @@
     enable = true;
     package = pkgs.vscodium;
     mutableExtensionsDir = false;
-    extensions =
-      let
-        extensions = vscode-extensions.extensions.${pkgs.system};
-      in
-      with extensions.vscode-marketplace;
-      with extensions.open-vsx;
-      [
-        activitywatch.aw-watcher-vscode
-        yanivmo.navi-cheatsheet-language
-        jnoortheen.nix-ide
-      ];
-    userSettings = {
-      "editor.fontFamily" = "Iosevka Curly Slab";
-      "editor.fontLigatures" = true;
-      "editor.rulers" = [ 80 ];
-      "window.commandCenter" = false;
-      "window.titleBarStyle" = "custom";
+    profiles.default = {
+      extensions =
+        let
+          extensions = vscode-extensions.extensions.${pkgs.system};
+        in
+        with extensions.vscode-marketplace;
+        with extensions.open-vsx;
+        [
+          activitywatch.aw-watcher-vscode
+          yanivmo.navi-cheatsheet-language
+          jnoortheen.nix-ide
+        ];
+      userSettings = {
+        "editor.fontFamily" = "Iosevka Curly Slab";
+        "editor.fontLigatures" = true;
+        "editor.rulers" = [ 80 ];
+        "window.commandCenter" = false;
+        "window.titleBarStyle" = "custom";
 
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nixd";
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+      };
     };
   };
 }
