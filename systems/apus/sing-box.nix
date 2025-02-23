ecc850fc1c8ad4e16b59c58b1f125a97f1d33a31
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-23 20:37:50 +0800

    feat(systems/apus/sing-box): set dns

--- systems/apus/sing-box.nix
+++ systems/apus/sing-box.nix
@@ -7,6 +7,23 @@
       log = {
         level = "info";
       };
+      dns = {
+        servers = [
+          {
+            type = "https";
+            server = "dns.google";
+            domain_resolver = "alidns";
+            detour = "proxy";
+          }
+          {
+            type = "https";
+            tag = "alidns";
+            server = "223.5.5.5";
+            detour = "direct";
+          }
+        ];
+        reverse_mapping = true;
+      };
     };
   };
 }
