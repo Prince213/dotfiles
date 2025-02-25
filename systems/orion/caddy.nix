97e149dca03d781b54d0ad058f2efff6f3da1cc9
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-26 04:23:25 +0800

    feat(systems/orion/caddy): add file server

--- systems/orion/caddy.nix
+++ systems/orion/caddy.nix
@@ -1,5 +1,17 @@
 {
+  networking.firewall.allowedTCPPorts = [
+    80
+  ];
+
   services.caddy = {
     enable = true;
+    virtualHosts = {
+      "http://" = {
+        extraConfig = ''
+          root * /srv
+          file_server
+        '';
+      };
+    };
   };
 }
