54e5aaf99f57033982e25d1a4a8ca57af484f188
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-27 15:41:11 +0800

    feat(systems/nun/networking): add 10-eth0

--- systems/nun/networking.nix
+++ systems/nun/networking.nix
@@ -1,6 +1,16 @@
+{ config, ... }:
 {
   networking.useNetworkd = true;
   systemd.network = {
     enable = true;
+    networks = {
+      "10-eth0" = {
+        name = "eth0";
+      };
+    };
+  };
+
+  environment.etc = {
+    "systemd/network/10-eth0.network.d/10-network.conf".source = config.sops.secrets.network-eth0.path;
   };
 }
