d64e65b6845a6cc3d54baa6a9dfc1d0f1e8c63d9
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-27 15:49:27 +0800

    feat(systems/nun/users): set root ssh key

--- systems/nun/users.nix
+++ systems/nun/users.nix
@@ -3,6 +3,9 @@
   users.users = {
     root = {
       hashedPasswordFile = config.sops.secrets.root-password-hash.path;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9XJK666gs+w68DxJsv0UDin4lrBDfwExEzmOpxsMul"
+      ];
     };
   };
 }
