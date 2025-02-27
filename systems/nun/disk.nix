f0d32f6f68ece0f97a419edb9898996951e95add
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-27 13:46:48 +0800

    feat(systems/nun/disk): add disk main

--- systems/nun/disk.nix
+++ systems/nun/disk.nix
@@ -1,6 +1,26 @@
 {
   disko.devices = {
-    disk = {
+    disk.main = {
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            priority = 1;
+            type = "ef02";
+            size = "1M";
+          };
+          root = {
+            priority = 2;
+            type = "8304";
+            size = "100%";
+            content = {
+              type = "btrfs";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
     };
   };
 }
