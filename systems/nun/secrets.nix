d39d73432c8d6ce6f52c21f680ebfd4a849aee41
Sizhe Zhao <prc.zhao@outlook.com>
2025-02-27 23:08:23 +0800

    feat(systems/nun/secrets): fix network-eth0 permission

--- systems/nun/secrets.nix
+++ systems/nun/secrets.nix
@@ -4,7 +4,9 @@
       root-password-hash = {
         neededForUsers = true;
       };
-      network-eth0 = { };
+      network-eth0 = {
+        mode = "0444";
+      };
     };
 
     defaultSopsFile = ./secrets.yaml;
